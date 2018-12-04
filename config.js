ac891295b9fbc91a963acdaa777d76345868ec5d
Dean <mccarthey123@outlook.com>
2018-12-04 19:05:01 +0800

    delete config

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	baseUrl: 'https://spacdn.hellobyebye.com/',
-    QN_ACCESS_KEY: 'gUj40toJvkFqlAHxWuegsy1tWq6C7yzPzxCvKiBs',
-    QN_SECRET_KEY: 'nr8QO7TDfQJIslWtLeW1NLYnKWjKtwLONDfd4RDb',
-    QN_BUCKET: 'spacdn',
-}
